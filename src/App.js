d08763bba57b89397e0fa0f3314e1d2c6be6e57d
zyko <zyko.taouaf@gmail.com>
2024-06-06 10:00:00 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState();
+  const [posts, setPosts] = useState();
+  const [postsActiveUser, setPostsActiveUser] = useState();
+  const [activeId, setActiveId] = useState();
+
+  console.log(users, posts);
+
+  const gettingUsers = () => {
+    const users = axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data));
+  };
+
+  const gettingPosts = () => {
+    const posts = axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setPosts(response.data));
+  };
+
+  const showPosts = (id) => {
+    setActiveId(id);
+    const filteredPosts = posts.filter((post) => post.id == id);
+    console.log(filteredPosts);
+    setPostsActiveUser(filteredPosts);
+  };
+
+  useEffect(() => {
+    gettingUsers();
+    gettingPosts();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users?.map((user, key) => (
+        <div>
+          {user.name}
+          <button onClick={() => showPosts(user.id)}>Afficher posts</button>
+          {activeId == user.id &&
+            postsActiveUser.map((post, key) => (
+              <div>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+                <button onClick={() => setActiveId(null)}>Hide</button>
+              </div>
+            ))}
+        </div>
+      ))}
     </div>
   );
 }
