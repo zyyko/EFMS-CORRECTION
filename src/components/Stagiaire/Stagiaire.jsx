ed0514b7f81ee9166b6344b533e166ec104a5ea5
zyko <zyko.taouaf@gmail.com>
2024-06-07 22:56:59 +0100

    done

--- src/components/Stagiaire/Stagiaire.jsx
+++ src/components/Stagiaire/Stagiaire.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./Stagiaire.module.css";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
 export default function Stagiaire({ stagiaire }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = (nom) => {
+    dispatch({ type: "DELETE_STAGIAIRE", payload: nom });
+  };
   return (
     <div className={styles.stagiaireContainer}>
       <NavLink to={"/stagiaire/" + stagiaire.nom}>
         <img src={stagiaire.image} />
       </NavLink>
       <h3>{stagiaire.nom}</h3>
+      <button onClick={() => handleDelete(stagiaire.nom)}>Delete</button>
     </div>
   );
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,6 +4,8 @@ export default function reducer(state, action) {
   switch (action.type) {
     case "AJOUTER_STAGIAIRE":
       return [...state, action.payload];
+    case "DELETE_STAGIAIRE":
+      return state.filter((stagiaire) => stagiaire.nom !== action.payload);
     default:
       return state;
   }
